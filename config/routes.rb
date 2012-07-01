b46f1f96743645295df139bfb7f14e57812a2183
Ian Bibby <ian.bibby@gmail.com>
2012-07-01 00:47:36 -0400

    Finished user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
-  match '/signup', to: "users#new"
-
   root to: 'static_pages#home'
+
+  resources :users
+
+  match '/signup', to: "users#new"
   match '/home',    to: "static_pages#home"
   match '/help',    to: "static_pages#help"
   match '/about',   to: "static_pages#about"
